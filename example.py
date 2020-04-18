8c200061df6f294d0a7ab48583bbe9fe3ff09f10
Helldog136 <130792@student.umons.ac.be>
2020-04-18 18:39:15 +0200

    added error logging

--- example.py
+++ example.py
@@ -11,12 +11,12 @@ CLIENT_SECRET = 'tYKqSKbmjw3hKsoNtaaKKtXXP0sa' # Put your openapi client secret 
 
 
 async def go(LOOP):
-    stop_name = "scherdemael"
+    stop_name = "Scherdemael"
     lines_filter = [(46, "Glibert")]
     custom_session = aiohttp.ClientSession()
     APIClient = STIBAPIClient(LOOP, custom_session, CLIENT_ID, CLIENT_SECRET)
     service = STIBService(APIClient)
-    print(await service.get_passages(stop_name, lines_filter))
+    print(await service.get_passages(stop_name, lines_filter, max_passages=3))
     await custom_session.close()
 
 
--- pystibmvib/service/STIBService.py
+++ pystibmvib/service/STIBService.py
@@ -24,14 +24,15 @@ class STIBService:
             line_filter_nr = [k[0] for k in line_filters]
             line_filter_dest = [k[1].upper() for k in line_filters]
             atomic_stop_infos = filter(
-                (lambda s: s.get_line_nr() in line_filter_nr and s.get_destination().upper() == line_filter_dest[line_filter_nr.index(s.get_line_nr())]),
+                (lambda s: s.get_line_nr() in line_filter_nr and s.get_destination().upper() == line_filter_dest[
+                    line_filter_nr.index(s.get_line_nr())]),
                 atomic_stop_infos)
         passages = []
         for atomic in atomic_stop_infos:
             call_url_suffix = PASSING_TIME_BY_POINT_SUFFIX + atomic.get_stop_id()
 
-            raw_passages = await self.api_client.api_call(call_url_suffix)
-            raw_passages = json.loads(raw_passages)
+            raw_str_passages = await self.api_client.api_call(call_url_suffix)
+            raw_passages = json.loads(raw_str_passages)
             for point in raw_passages["points"]:
                 for json_passage in point["passingTimes"]:
                     if len(passages) >= max_passages:
@@ -39,14 +40,18 @@ class STIBService:
                     message = ""
                     try:
                         message = json_passage["message"][lang]
-                    except:
+                    except KeyError as ke:
                         pass
-                    passages.append(Passage(stop_id=point["pointId"],
-                                            lineId=json_passage["lineId"],
-                                            destination=json_passage["destination"][lang],
-                                            expectedArrivalTime=json_passage["expectedArrivalTime"],
-                                            lineInfos=await self._shapefile_service.get_line_info(
-                                                json_passage["lineId"]),
-                                            message=message,
-                                            now=now))
+                    try:
+                        passages.append(Passage(stop_id=point["pointId"],
+                                                lineId=json_passage["lineId"],
+                                                destination=json_passage["destination"][lang],
+                                                expectedArrivalTime=json_passage["expectedArrivalTime"],
+                                                lineInfos=await self._shapefile_service.get_line_info(
+                                                    json_passage["lineId"]),
+                                                message=message,
+                                                now=now))
+                    except KeyError as ke:
+                        LOGGER.error("Error while parsing response from STIB. Raw response is: " + raw_passages)
+                        raise ke
         return passages
