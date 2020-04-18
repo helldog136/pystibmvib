80132bfb6303aee8177a865abdcfb54d41e77beb
Helldog136 <130792@student.umons.ac.be>
2020-04-18 10:22:17 +0200

    package reorganization, made passages object serializable by inheriting from dict. Renamed some fields from camelCase to underscore_separated_words

--- pystibmvib/domain/passages.py
+++ pystibmvib/domain/passages.py
@@ -1,29 +1,20 @@
 from datetime import *
 
 from .line import LineInfo
-from ..util.util import get_time_delta
+from .util import get_time_delta
 
 
-class Passage:
-    def __init__(self, stop_id: int, lineId: int, destination: str, expectedArrivalTime: str, lineInfos: LineInfo, message:str = "", now: datetime = datetime.now()):
-        self.stop_id = stop_id
-        self.lineId = lineId
-        self.destination = destination
-        self.expectedArrivalTime = expectedArrivalTime
-        self.lineInfos = lineInfos
-        self.message = message
+class Passage(dict):
+    def __init__(self, stop_id: int, lineId: int, destination: str, expectedArrivalTime: str, lineInfos: LineInfo,
+                 message: str = "", now: datetime = datetime.now()):
+        dict.__init__(self)
+        self["stop_id"] = stop_id
+        self["line_id"] = lineId
+        self["destination"] = destination
+        self["expected_arrival_time"] = expectedArrivalTime
+        self["line_color"] = lineInfos.get_line_color()
+        self["line_type"] = lineInfos.get_line_type()
+        self["message"] = message
         delta = get_time_delta(now, expectedArrivalTime)
-        self.arriving_in = {"min": delta // 60, "sec": delta % 60}
-        self._dict = self.to_dict()
+        self["arriving_in"] = {"min": delta // 60, "sec": delta % 60}
 
-    def to_dict(self):
-        return {"stop_id": self.stop_id, "lineId": self.lineId, "destination": self.destination,
-                "expectedArrivalTime": self.expectedArrivalTime, "arriving_in": self.arriving_in,
-                "message": self.message,
-                "line_color": self.lineInfos.get_line_color(), "line_type": self.lineInfos.get_line_type()}
-
-    def __str__(self):
-        return str(self.to_dict())
-
-    def __repr__(self):
-        return self.__str__()
--- tests/pystibmvib/test_api_passages.py
+++ tests/pystibmvib/test_api_passages.py
@@ -1,11 +1,12 @@
 """Example usage of pystibmvib."""
 import asyncio
 import datetime
+import json
 import unittest
 
 import aiohttp
 
-from pystibmvib import STIBService
+from pystibmvib import STIBService, Passage, LineInfo
 from tests.pystibmvib import MockAPIClient
 
 
@@ -32,12 +33,47 @@ class TestPassages(unittest.TestCase):
             delta2 = datetime.timedelta(minutes=13, seconds=22)
 
             # Check message
-            self.assertEqual(passages[0].message, "foofr")
-            self.assertEqual(passages[1].message, "")
+            self.assertEqual(passages[0]["message"], "foofr")
+            self.assertEqual(passages[1]["message"], "")
 
             await custom_session.close()
 
         self.LOOP.run_until_complete(go(self.LOOP))
+
+    def test_serializable(self):
+        async def go(LOOP):
+            stop_name = "scherdemael"
+            lines_filter = [(46, "Glibert")]
+            custom_session = aiohttp.ClientSession()
+
+            APIClient = MockAPIClient()
+
+            service = STIBService(APIClient)
+            passages = await service.get_passages(stop_name, lines_filter)
+
+            print(json.dumps(passages))
+
+            await custom_session.close()
+
+        self.LOOP.run_until_complete(go(self.LOOP))
+
+    def test_atomic_passage_serialization(self):
+        now = datetime.datetime.now()
+        delta1 = datetime.timedelta(minutes=3, seconds=25)
+        p = Passage(stop_id=42, lineId=21, destination="FooDest",
+                    expectedArrivalTime=(now + delta1).strftime("%Y-%m-%dT%H:%M:%S"),
+                    lineInfos=LineInfo(line_nr=21, line_type="B", line_color="#ffffff"), message="FooMsg", now=now)
+
+        js = json.loads(json.dumps(p))
+        self.assertEqual(js["stop_id"], 42)
+        self.assertEqual(js["line_id"], 21)
+        self.assertEqual(js["destination"], "FooDest")
+        self.assertEqual(js["expected_arrival_time"], (now + delta1).strftime("%Y-%m-%dT%H:%M:%S"))
+        self.assertEqual(js["line_color"], "#ffffff")
+        self.assertEqual(js["line_type"], "B")
+        self.assertEqual(js["message"], "FooMsg")
+        self.assertEqual(js["arriving_in"]["min"], 3)
+        self.assertEqual(js["arriving_in"]["sec"], 24)
 
 
 if __name__ == '__main__':
